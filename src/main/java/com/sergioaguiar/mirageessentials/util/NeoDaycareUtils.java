b4414f61c722967a4dbce3f1adea09904b863ed2
sergio-aguiar <pioavenger@gmail.com>
2026-08-15 20:36:31 +0100

    Removed NeoDaycare dependency code in favor of reflection; fixed egg check

--- src/main/java/com/sergioaguiar/mirageessentials/util/NeoDaycareUtils.java
+++ src/main/java/com/sergioaguiar/mirageessentials/util/NeoDaycareUtils.java
@@ -1,15 +1,13 @@
 package com.sergioaguiar.mirageessentials.util;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
-import com.cobblemon.mod.common.api.pokemon.PokemonProperties;
 import com.cobblemon.mod.common.api.pokemon.feature.IntSpeciesFeature;
 import com.cobblemon.mod.common.api.pokemon.feature.SpeciesFeature;
 import com.cobblemon.mod.common.pokemon.Pokemon;
 import com.sergioaguiar.mirageessentials.config.chatparser.aspects.ChatAspects;
 
-import dev.neovitalism.neodaycare.config.MainConfig;
-import dev.neovitalism.neodaycare.utils.DaycareUtils;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.nbt.NbtCompound;
 
@@ -17,13 +15,35 @@ public class NeoDaycareUtils
 {
     private static final String MOD_ID_STRING = "neodaycare";
 
-    private static String EGG_PROPERTY_STRING = "egg";
+    private static final String DAYCAREUTILS_CLASS_STRING = "dev.neovitalism.neodaycare.utils.DaycareUtils";
+    private static final String MAINCONFIG_CLASS_STRING = "dev.neovitalism.neodaycare.config.MainConfig";
 
-    private static final PokemonProperties EGG_PROPERTY;
+    private static Class<?> daycareUtilsClass;
+    private static Class<?> mainConfigClass;
+
+    private static Method isUnbreedableMethod;
+    private static Method isEggMethod;
+    private static Method getStepsPerCycleMethod;
+
+    private static boolean isNeodaycareAvailable;
 
     static 
     {
-        EGG_PROPERTY = PokemonProperties.Companion.parse(EGG_PROPERTY_STRING);
+        try
+        {
+            daycareUtilsClass = Class.forName(DAYCAREUTILS_CLASS_STRING);
+            mainConfigClass = Class.forName(MAINCONFIG_CLASS_STRING);
+            
+            isUnbreedableMethod = daycareUtilsClass.getDeclaredMethod("isUnbreedable", Pokemon.class);
+            isEggMethod = daycareUtilsClass.getDeclaredMethod("isEgg", Pokemon.class);
+            getStepsPerCycleMethod = mainConfigClass.getDeclaredMethod("getStepsPerCycle");
+            
+            isNeodaycareAvailable = true;
+        }
+        catch (Exception e)
+        {
+            isNeodaycareAvailable = false;
+        }
     }
 
     public static boolean isModLoaded()
@@ -34,42 +54,68 @@ public class NeoDaycareUtils
     public static boolean isNeutered(Pokemon pokemon)
     {
         if (pokemon == null) return false;
+        if (!isNeodaycareAvailable) return getIsNeuteredFallback(pokemon);
+        return tryIsUnbreedable(pokemon);
+    }
 
+    private static boolean tryIsUnbreedable(Pokemon pokemon)
+    {
         try
         {
-            return DaycareUtils.isUnbreedable(pokemon);
+            return (boolean) isUnbreedableMethod.invoke(null, pokemon);
         }
         catch (Exception e)
         {
-            return pokemon.getPersistentData().contains("breedable") && !pokemon.getPersistentData().getBoolean("breedable");
+            return getIsNeuteredFallback(pokemon);
         }
+    }
+
+    private static boolean getIsNeuteredFallback(Pokemon pokemon)
+    {
+        return pokemon.getPersistentData().contains("breedable") && !pokemon.getPersistentData().getBoolean("breedable");
     }
 
     public static boolean isEgg(Pokemon pokemon)
     {
         if (pokemon == null) return false;
+        if (!isNeodaycareAvailable) return getIsEggFallback(pokemon);
+        return tryIsEgg(pokemon);
+    }
 
+    private static boolean tryIsEgg(Pokemon pokemon)
+    {
         try
         {
-            return DaycareUtils.isEgg(pokemon);
+            return (boolean) isEggMethod.invoke(null, pokemon);
         }
         catch (Exception e)
         {
-            return EGG_PROPERTY.matches(pokemon);
+            return getIsEggFallback(pokemon);
         }
+    }
+
+    private static boolean getIsEggFallback(Pokemon pokemon)
+    {
+        return false;
     }
 
     public static Integer getRemainingSteps(Pokemon pokemon)
     {
         if (pokemon == null || !isEgg(pokemon)) return null;
+        if (!isNeodaycareAvailable) return null;
+        return tryGetRemainingSteps(pokemon);
+    }
 
+    private static Integer tryGetRemainingSteps(Pokemon pokemon)
+    {
         try
         {
             NbtCompound data = pokemon.getPersistentData();
             int cycle = data.getInt("Cycle");
             int steps = data.getInt("Steps");
+            int stepsPerCycle = (int) getStepsPerCycleMethod.invoke(null);
 
-            return (cycle - 1) * MainConfig.getStepsPerCycle() + steps;
+            return (cycle - 1) * stepsPerCycle + steps;
         }
         catch (Exception e)
         {
